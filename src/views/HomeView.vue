ad2365305e0afaa891c17f25b4566fece41afc6a
Lisa Copeland <lisa.copeland@betterworks.com>
2023-02-01 19:42:13 -0500

    updated styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,3 @@
-<script setup>
-import { ref, onMounted, computed, watch } from "vue";
-
-const todos = ref([]);
-const name = ref("");
-
-const input_content = ref("");
-const input_category = ref(null);
-
-const todos_asc = computed(() =>
-  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-  todos.value.sort((a, b) => {
-    return a.createdAt - b.createdAt;
-  })
-);
-
-watch(name, (newVal) => {
-  localStorage.setItem("name", newVal);
-});
-
-watch(
-  todos,
-  (newVal) => {
-    localStorage.setItem("todos", JSON.stringify(newVal));
-  },
-  {
-    deep: true,
-  }
-);
-
-const addTodo = () => {
-  if (input_content.value.trim() === "" || input_category.value === null) {
-    return;
-  }
-
-  todos.value.push({
-    content: input_content.value,
-    category: input_category.value,
-    done: false,
-    editable: false,
-    createdAt: new Date().getTime(),
-  });
-};
-
-const removeTodo = (todo) => {
-  todos.value = todos.value.filter((t) => t !== todo);
-};
-
-onMounted(() => {
-  name.value = localStorage.getItem("name") || "";
-  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
-});
-</script>
-
 <template>
   <main class="app">
     <section class="greeting">
@@ -134,3 +80,57 @@ onMounted(() => {
     </section>
   </main>
 </template>
+
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  {
+    deep: true,
+  }
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    editable: false,
+    createdAt: new Date().getTime(),
+  });
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
+</script>
